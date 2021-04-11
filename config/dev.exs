cabe9ab1a9d88948e75ed16c80d1e261372382d1
amy null <null@amy.gg>
2021-04-11 00:05:39 -0400

    Switch to a more-interesting port

--- config/dev.exs
+++ config/dev.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :crush, CrushWeb.Endpoint,
-  http: [port: String.to_integer(System.get_env("PORT") || "4000")],
+  http: [port: String.to_integer(System.get_env("PORT") || "7654")],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
