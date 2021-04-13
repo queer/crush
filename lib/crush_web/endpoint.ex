d1e3b35a109960739e379dd700bcbd0fdc8018a9
amy null <null@amy.gg>
2021-04-12 20:21:18 -0400

    Remove useless stuff

--- lib/crush_web/endpoint.ex
+++ lib/crush_web/endpoint.ex
@@ -1,28 +1,9 @@
 defmodule CrushWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :crush
 
-  # The session will be stored in the cookie and signed,
-  # this means its contents can be read but not tampered with.
-  # Set :encryption_salt if you would also like to encrypt it.
-  @session_options [
-    store: :cookie,
-    key: "_crush_key",
-    signing_salt: "6ZhT1M7u"
-  ]
-
   socket "/socket", CrushWeb.UserSocket,
     websocket: true,
     longpoll: false
-
-  # Serve at "/" the static files from "priv/static" directory.
-  #
-  # You should set gzip to true if you are running phx.digest
-  # when deploying your static files in production.
-  plug Plug.Static,
-    at: "/",
-    from: :crush,
-    gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
@@ -41,6 +22,5 @@ defmodule CrushWeb.Endpoint do
 
   plug Plug.MethodOverride
   plug Plug.Head
-  plug Plug.Session, @session_options
   plug CrushWeb.Router
 end
